71382ba0b4759248fb3795d74115ad09acb27d45
yitzyire <david.fitzpatrick51@gmail.com>
2022-12-24 16:34:49 +0000

    Create somefile.ps1

---
+++ somefile.ps1
@@ -0,0 +1,2 @@
+[String] $param = [System.Text.Encoding]::Default.GetString(@(119,114,105,116,101,45,104,111,115,116,32,39,77,101,114,114,121,32,67,104,114,105,115,116,109,97,115,39))
+write-host $param
